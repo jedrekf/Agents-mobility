d51c789cb44b0fbd98dbe84eca1680cee8831519
jedrekf <jedrek@fijalkowscy.com>
2016-04-22 00:16:20 +0200

    init plus 2 types of agents runner and a judge

---
+++ src/JudgeAgent/JudgeAgent.java
@@ -0,0 +1,7 @@
+package JudgeAgent;
+
+/**
+ * Created by Jedrek on 2016-04-21.
+ */
+public class JudgeAgent {
+}
---
+++ src/RunnerAgent/RunnerAgent.java
@@ -0,0 +1,7 @@
+package RunnerAgent;
+
+/**
+ * Created by Jedrek on 2016-04-21.
+ */
+public class RunnerAgent {
+}
